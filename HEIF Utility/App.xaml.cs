240e410082fffa827de382c8420c2d06a5fb47f5
liuziangexit <liuziang@liuziangexit.com>
2018-04-30 19:47:09 +0800

    update

---
+++ HEIF Utility/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.IO;
+
+namespace HEIF_Utility_HiDPI
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ HEIF Utility/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HEIF_Utility_HiDPI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string RunningLanguage;
+        public static string language_conf = "conf/Language";
+
+        public MainWindow()
+        {
+            ChangeLanguage(GetSystemLanguage());
+            InitializeComponent();
+            ChangeLanguageStatus();
+        }
+
+        private static ResourceDictionary GetLanguage(string Language)
+        {
+            if (Language == "zh-CN")
+                return new ResourceDictionary() { Source = new Uri("lang_zh-CHS.xaml", UriKind.RelativeOrAbsolute) };
+            else
+               if (Language.IndexOf("zh") != -1)
+                return new ResourceDictionary() { Source = new Uri("lang_zh-CHT.xaml", UriKind.RelativeOrAbsolute) };
+            else
+                return new ResourceDictionary() { Source = new Uri("lang_en-US.xaml", UriKind.RelativeOrAbsolute) };
+        }
+
+        private static ResourceDictionary GetSystemLanguage()
+        {
+            string SystemLanguage = null;
+            try
+            {
+                SystemLanguage = File.ReadAllText(language_conf);
+            }
+            catch (Exception)
+            {
+                SystemLanguage = System.Globalization.CultureInfo.InstalledUICulture.Name;
+            }
+            return GetLanguage(SystemLanguage);
+        }
+
+        private void ChangeLanguage(ResourceDictionary new_lang)
+        {
+            App.Current.Resources.MergedDictionaries.Remove(App.Current.Resources.MergedDictionaries.ToList().Find((ResourceDictionary rd) =>
+              {
+                  if (rd.Source.ToString().IndexOf("lang_") != -1)
+                      return true;
+                  return false;
+              }));
+            App.Current.Resources.MergedDictionaries.Add(new_lang);
+            RunningLanguage = new_lang.Source.ToString();
+        }
+
+        private void ChangeLanguageStatus()
+        {
+            enUS.IsChecked = zhCHS.IsChecked = zhCHT.IsChecked = false;
+            switch (RunningLanguage)
+            {
+                case "lang_en-US.xaml": enUS.IsChecked = true; break;
+                case "lang_zh-CHS.xaml": zhCHS.IsChecked = true; break;
+                case "lang_zh-CHT.xaml": zhCHT.IsChecked = true; ; break;
+            }
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("打开");
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("保存");
+        }
+
+        private void Copy_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("复制");
+        }
+        
+        private void StartBatchConversion(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("批量转换");
+        }
+
+        private void StartOnlineSupport(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("在线支持");
+        }
+
+        private void StartAbout(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("关于");
+        }
+
+        private void lang_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((MenuItem)sender).Name)
+            {
+                case "enUS":
+                    {
+                        ChangeLanguage(GetLanguage("en-US"));
+                        try
+                        {
+                            File.WriteAllText(language_conf, "en-US");
+                        }
+                        catch (Exception) { }
+                    }
+                    break;
+                case "zhCHS":
+                    {
+                        ChangeLanguage(GetLanguage("zh-CN"));
+                        try
+                        {
+                            File.WriteAllText(language_conf, "zh-CN");
+                        }
+                        catch (Exception) { }
+                    }
+                    break;
+                case "zhCHT":
+                    {
+                        ChangeLanguage(GetLanguage("zh-Tr"));
+                        try
+                        {
+                            File.WriteAllText(language_conf, "zh-Tr");
+                        }
+                        catch (Exception) { }
+                    }
+                    break;
+            }
+            ChangeLanguageStatus();
+        }
+
+        private void use_sys_lang_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                File.Delete(language_conf);
+            }
+            catch (Exception) { }
+            ChangeLanguage(GetSystemLanguage());
+            ChangeLanguageStatus();
+        }
+    }
+}
